995e925f001074608f0ba7b88add9465df53cc70
onifernando1 <onifernando1@gmail.com>
2022-07-02 17:45:59 +0100

    Create player class show name get name

--- ticatactoe2.rb
+++ ticatactoe2.rb
@@ -1,14 +1,62 @@
+
 class Game 
 
+    attr_accessor  :player_one_name
+    attr_accessor :player_two_name
+
+
+    def initialize()
+        @board = [1,2,3,4,5,6,7,8,9]
+        @@player_one_name = ''
+        @@player_two_name = ''
+        
+    end 
+
+    def show_name
+        puts @@player_one_name
+        puts @@player_two_name
+    end 
+
+    def draw_board
+        horizontal_divider = "----------"
+        puts "#{@board[0]} | #{@board[1]} | #{@board[2]}"
+        puts horizontal_divider
+        puts "#{@board[3]} | #{@board[4]} | #{@board[5]}"
+        puts horizontal_divider
+        puts "#{@board[6]} | #{@board[7]} | #{@board[8]}"
+    end 
+
+    def play_game
+        puts "Select your move #{@player_one_name}"
+
+    end 
+
+
 end 
 
-class Player
+class Player < Game
+    
+    def get_name
+        puts 'What is the name of Player 1? '
+        @@player_one_name = gets.chomp
+        puts 'What is the name of Player 2? '
+        @@player_two_name = gets.chomp
+        
+    end 
+
+    
+    
+    def assign_symbol
+        @p1_symbol = "X"
+        @p2_symobl = "O"
+    end 
 end 
 
-board = [1,2,3,4,5,6,7,8,9]
-horizontal_divider = "----------"
-puts "#{board[0]} | #{board[1]} | #{board[2]}"
-puts horizontal_divider
-puts "#{board[3]} | #{board[4]} | #{board[5]}"
-puts horizontal_divider
-puts "#{board[6]} | #{board[7]} | #{board[8]}"
+
+
+game = Game.new()
+game.play_game()
+player = Player.new()
+player.get_name
+player.assign_symbol()
+game.show_name()
