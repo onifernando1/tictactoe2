978ca2cc4b108fb7373d421a02e99f3b55207fc4
onifernando1 <onifernando1@gmail.com>
2022-07-02 17:47:01 +0100

    Change attr accessor to reader only for player names

--- ticatactoe2.rb
+++ ticatactoe2.rb
@@ -1,62 +1,50 @@
+# frozen_string_literal: true
 
-class Game 
+class Game
+  attr_reader :player_one_name, :player_two_name
 
-    attr_accessor  :player_one_name
-    attr_accessor :player_two_name
+  def initialize
+    @board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    @@player_one_name = ''
+    @@player_two_name = ''
+  end
 
+  def show_name
+    puts @@player_one_name
+    puts @@player_two_name
+  end
 
-    def initialize()
-        @board = [1,2,3,4,5,6,7,8,9]
-        @@player_one_name = ''
-        @@player_two_name = ''
-        
-    end 
+  def draw_board
+    horizontal_divider = '----------'
+    puts "#{@board[0]} | #{@board[1]} | #{@board[2]}"
+    puts horizontal_divider
+    puts "#{@board[3]} | #{@board[4]} | #{@board[5]}"
+    puts horizontal_divider
+    puts "#{@board[6]} | #{@board[7]} | #{@board[8]}"
+  end
 
-    def show_name
-        puts @@player_one_name
-        puts @@player_two_name
-    end 
-
-    def draw_board
-        horizontal_divider = "----------"
-        puts "#{@board[0]} | #{@board[1]} | #{@board[2]}"
-        puts horizontal_divider
-        puts "#{@board[3]} | #{@board[4]} | #{@board[5]}"
-        puts horizontal_divider
-        puts "#{@board[6]} | #{@board[7]} | #{@board[8]}"
-    end 
-
-    def play_game
-        puts "Select your move #{@player_one_name}"
-
-    end 
-
-
-end 
+  def play_game
+    puts "Select your move #{@player_one_name}"
+  end
+end
 
 class Player < Game
-    
-    def get_name
-        puts 'What is the name of Player 1? '
-        @@player_one_name = gets.chomp
-        puts 'What is the name of Player 2? '
-        @@player_two_name = gets.chomp
-        
-    end 
+  def get_name
+    puts 'What is the name of Player 1? '
+    @@player_one_name = gets.chomp
+    puts 'What is the name of Player 2? '
+    @@player_two_name = gets.chomp
+  end
 
-    
-    
-    def assign_symbol
-        @p1_symbol = "X"
-        @p2_symobl = "O"
-    end 
-end 
+  def assign_symbol
+    @p1_symbol = 'X'
+    @p2_symobl = 'O'
+  end
+end
 
-
-
-game = Game.new()
-game.play_game()
-player = Player.new()
+game = Game.new
+game.play_game
+player = Player.new
 player.get_name
-player.assign_symbol()
-game.show_name()
+player.assign_symbol
+game.show_name
