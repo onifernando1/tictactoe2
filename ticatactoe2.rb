be55c290044c2fe46b16e457b07e0e07f00be2e8
onifernando1 <onifernando1@gmail.com>
2022-07-02 14:14:26 +0100

    Create board

---
+++ ticatactoe2.rb
@@ -0,0 +1,14 @@
+class Game 
+
+end 
+
+class Player
+end 
+
+board = [1,2,3,4,5,6,7,8,9]
+horizontal_divider = "----------"
+puts "#{board[0]} | #{board[1]} | #{board[2]}"
+puts horizontal_divider
+puts "#{board[3]} | #{board[4]} | #{board[5]}"
+puts horizontal_divider
+puts "#{board[6]} | #{board[7]} | #{board[8]}"
