be06c7142dfcbbd8a4ebd3bbedf5e999a74caf5a
onifernando1 <onifernando1@gmail.com>
2022-07-03 15:59:48 +0100

    Add draw, stop turn when someone wns

--- ticatactoe2.rb
+++ ticatactoe2.rb
@@ -7,6 +7,8 @@ class Game
     @@board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
     @@player_one_name = ''
     @@player_two_name = ''
+    @p1_win = false 
+    @p2_win = false 
   end
 
   def show_name
@@ -22,11 +24,104 @@ class Game
     puts horizontal_divider
     puts "#{@@board[6]} | #{@@board[7]} | #{@@board[8]}"
   end
+  
+  def check_win
 
+    # Horizontal rows 
+
+    if @@board[0] == "X" && @@board[1] == "X" && @@board[2] == "X"
+      @p1_win = true 
+      puts "#{@@player_one_name} wins"
+     
+
+    elsif @@board[0] == "O" && @@board[1] == "O" && @@board[2] == "O"
+      @p2_win = true 
+      puts "#{@@player_two_name} wins"
+     
+
+    elsif @@board[3] == "X" && @@board[4] == "X" && @@board[5] == "X"
+      @p1_win = true 
+      puts "#{@@player_one_name} wins"
+     
+
+    elsif @@board[3] == "O" && @@board[4] == "O" && @@board[5] == "O"
+      @p2_win = true 
+      puts "#{@@player_two_name} wins"
+     
+
+    elsif @@board[6] == "X" && @@board[7] == "X" && @@board[8] == "X"
+      @p1_win = true 
+      puts "#{@@player_one_name} wins"
+     
+
+    elsif @@board[6] == "O" && @@board[7] == "O" && @@board[8] == "O"
+      @p2_win = true 
+      puts "#{@@player_two_name} wins"
+    
+
+    #Vertical rows 
+
+    elsif @@board[0] == "X" && @@board[3] == "X" && @@board[6] == "X"
+      @p1_win = true 
+      puts "#{@@player_one_name} wins"
+     
+
+    elsif @@board[0] == "O" && @@board[3] == "O" && @@board[6] == "O"
+      @p2_win = true 
+      puts "#{@@player_two_name} wins"
+    
+    elsif @@board[1] == "X" && @@board[4] == "X" && @@board[7] == "X"
+      @p1_win = true 
+      puts "#{@@player_one_name} wins" 
+
+    elsif @@board[1] == "O" && @@board[4] == "O" && @@board[7] == "O"
+      @p2_win = true 
+      puts "#{@@player_two_name} wins"
+
+    elsif @@board[2] == "X" && @@board[5] == "X" && @@board[8] == "X"
+      @p1_win = true 
+      puts "#{@@player_one_name} wins" 
+
+    elsif @@board[2] == "O" && @@board[5] == "O" && @@board[8] == "O"
+      @p2_win = true 
+      puts "#{@@player_two_name} wins"
+
+
+    #Diagonals 
+    elsif @@board[0] == "X" && @@board[4] == "X" && @@board[8] == "X"
+      @p1_win = true 
+      puts "#{@@player_one_name} wins" 
+
+    elsif @@board[0] == "O" && @@board[4] == "O" && @@board[8] == "O"
+      @p2_win = true 
+      puts "#{@@player_two_name} wins"
+
+    elsif @@board[2] == "X" && @@board[4] == "X" && @@board[6] == "X"
+      @p1_win = true 
+      puts "#{@@player_one_name} wins" 
+
+    elsif @@board[2] == "O" && @@board[4] == "O" && @@board[6] == "O"
+      @p2_win = true 
+      puts "#{@@player_two_name} wins"
+
+    #draw
+    
+    elsif @@board[0] != 1 && @@board[1] != 2 && @@board[2] != 3 && @@board[4] != 5 && @@board[5] != 6 && @@board[7] != 8 && @@board[8] != 9 && @p1_win == false && @p2_win == false
+      puts "DRAW"
+      @p1_win = true 
+      @p2_win = true
+    end 
+end 
 
   def player_1_move
+    check_win()
     @next_turn = false 
+    @round = 0
+    if @round == 0
+      draw_board()
+    end 
     until @next_turn == true 
+      @round += 1 
       puts "Select your move #{@@player_one_name}"
       @player_1_move = gets.chomp.to_i
       @player_1_index = (@player_1_move - 1).to_i 
@@ -37,9 +132,10 @@ class Game
       end 
     end 
     draw_board()
-    end 
+  end 
 
     def player_2_move
+        check_win()
         @next_turn = false 
         until @next_turn == true 
           puts "Select your move #{@@player_two_name}"
@@ -48,23 +144,25 @@ class Game
           if @@board[@player_2_index] != "X" && @@board[@player_2_index] != "O"
             @@board[@player_2_index] = "O"
             @next_turn = true 
-        else puts "INVALID SELECTION"  
-        end 
+          else puts "INVALID SELECTION"  
+          end 
         end 
 
-        draw_board()
+      draw_board()
     end
 
     def play_game
+        
+      until @p1_win == true || @p2_win == true 
         player_1_move()
+        check_win()
+        if @p1_win != true 
         player_2_move()
-        player_1_move()
-        player_2_move()
-         player_1_move()
-        player_2_move()
-    end
+        end 
+      end
+    end 
 
-    def check_win
+   
 
 end
 
@@ -76,15 +174,11 @@ class Player < Game
     @@player_two_name = gets.chomp
   end
 
-  def assign_symbol
-    @p1_symbol = 'X'
-    @p2_symobl = 'O'
-  end
+
 end
 
 game = Game.new
 player = Player.new
 player.get_name
-player.assign_symbol
 game.show_name
 game.play_game
